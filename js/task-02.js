c0e1765cf5ef47763df0deda5215d739bc2b32b9
Alexander Khytrych <san9hi4@gmail.com>
2023-02-27 00:24:24 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const ingredientsListItems = ingredients.map(ingredient => {
+  const ingredientItemEl = document.createElement('li');
+  ingredientItemEl.textContent = ingredient;
+  ingredientItemEl.classList.add('item');
+  return ingredientItemEl;
+})
+
+ingredientsListEl.append(...ingredientsListItems);
